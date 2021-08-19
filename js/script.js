1849c9aaf4ede93cfc7ac2bc601fab1aec5abf6f
zaktitov <zaktitov@gmail.com>
2021-08-19 12:17:25 +0300

    new

---
+++ js/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+  $(`.slider`).slick({
+    arrows: true, dots: true,
+    adaptiveHeight: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 900,
+    easing: "ease",
+    infinite: true,
+    initialSlide: 0,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    pauseOnFocus: true,
+    pauseOnHover:true,
+    pauseOnDotsHover: true,
+    draggable: true,
+    swipe: true,
+    touchTreshold: 5,
+    waitForAnimate: false,
+    centerMode: false,
+    variableWidth: false,
+    rows: false,
+    slidesPerRow: false,
+    vertical: false,
+    verticalSwiping:false,
+    
+    
+  })
+});
